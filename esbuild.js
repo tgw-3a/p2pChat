68f4ee2121b14eeb61a8adb59fff5260e0e637de
tgw-3a <tagawayuu@gmail.com>
2025-05-13 10:26:48 +0900

    UI/UXの向上

--- esbuild.js
+++ esbuild.js
@@ -1,10 +1,12 @@
 import path from 'path'
-import { build } from 'esbuild'
+import {build} from 'esbuild'
 
 build({
-  entryPoints: ['src/main/js/libp2p.js'],
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-  outfile: path.resolve(__dirname, 'src/main/resources/static/js/libp2p.js'),
+    entryPoints: ['src/main/resources/static/js/libp2p.js'],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    outfile: path.resolve(__dirname, 'src/main/resources/static/js/bundle.js'),
+    format: 'esm',
+    target: ['esnext']
 }).catch(() => process.exit(1))
